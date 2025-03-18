c0db52cd0267c7f1a11b0b7fee01111a1a4ab4ef
Jesus <jesustmontalvo@gmail.com>
2025-03-18 17:29:47 -0400

    dd

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 # Initialize Firebase
 try:
-    cred = credentials.Certificate("quizproject-a6230-firebase-adminsdk-fbsvc-0dce98ca5e.json")
+    cred = credentials.Certificate("quizproject-a6230-firebase-adminsdk-fbsvc-966ef715ea.json")
     if not firebase_admin._apps:
         firebase_admin.initialize_app(cred)
     db = firestore.client()
--- auth.py
+++ auth.py
@@ -6,7 +6,7 @@ import bcrypt
 
 # Initialize Firebase app
 if not firebase_admin._apps:
-    cred = credentials.Certificate("quizproject-a6230-firebase-adminsdk-fbsvc-0dce98ca5e.json")
+    cred = credentials.Certificate("quizproject-a6230-firebase-adminsdk-fbsvc-966ef715ea.json")
     firebase_admin.initialize_app(cred)
 
 auth_routes = Blueprint('auth', __name__)
