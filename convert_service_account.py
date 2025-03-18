7c135544bd02152631fe607fe44ee725b5244c7c
Jesus <jesustmontalvo@gmail.com>
2025-03-18 17:31:35 -0400

    hh

--- convert_service_account.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("serviceAccountKey.json") as f:
-    json_data = json.load(f)
-
-json_string = json.dumps(json_data)
-print(json_string)  # Copy this output so that the credentials can run on render
