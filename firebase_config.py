c4a8ec31b85cd1af9f7a929b83e5f455e424bcbf
extoiclng <jesustmontalvo@gmail.com>
2025-03-25 12:06:40 -0400

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,13 +1,20 @@
+from dotenv import load_dotenv
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Path to your service account key JSON file
-service_account_key_path = "quizproject-a6230-firebase-adminsdk-fbsvc-267213700c.json"
+# Load environment variables from .env
+load_dotenv()
+
+# Get environment variables
+service_account_key_path = os.getenv("QUIZPROJECT_SERVICE_ACCOUNT_KEY")
+firebase_project_id = os.getenv("QUIZPROJECT_PROJECT_ID")
 
 # Initialize Firebase
 if not firebase_admin._apps:
     cred = credentials.Certificate(service_account_key_path)
-    firebase_admin.initialize_app(cred)
+    firebase_admin.initialize_app(cred, {"projectId": firebase_project_id})
 
 # Firestore client
 db = firestore.client()
+service_account_key_path = os.path.join(os.path.dirname(__file__), "quizproject-a6230-firebase-adminsdk-fbsvc-6f3958fc54.json")
